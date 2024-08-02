3cd4865ec14249177b4ddf7de1ff7d7bb03466f7
JuanLoaiza007 <juan.loaiza.santiago@correounivalle.edu.co>
2024-08-02 02:06:43 -0500

    Revert "fix(env): change NEXT_PUBLIC type to SECRET for API"
    This reverts commit 57375cb92f94fbaa8dcb4c98220ad9d980cd3d29.

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.SECRET_API_KEY,
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
 
 export const obtenerSaldo = async (numeroTarjeta) => {
