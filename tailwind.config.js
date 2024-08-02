7d260b79df8bd336d57af1ed151349d26d434cf8
JuanLoaiza007 <juan.loaiza.santiago@correounivalle.edu.co>
2024-08-02 00:01:46 -0500

    feat(config): add blue color palette to Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "blue-ribbon": {
+          50: "#edfaff",
+          100: "#d6f1ff",
+          200: "#b5e9ff",
+          300: "#83ddff",
+          400: "#48c7ff",
+          500: "#1ea8ff",
+          600: "#068aff",
+          700: "#0070f3",
+          800: "#085ac5",
+          900: "#0d4e9b",
+          950: "#0e305d",
+        },
+      },
+    },
   },
   plugins: [],
 };
