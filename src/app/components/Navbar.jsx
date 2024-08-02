9c6e1ed98442375f1f9438f1469b2e6440ff9d81
JuanLoaiza007 <juan.loaiza.santiago@correounivalle.edu.co>
2024-08-02 09:07:45 -0500

    feat: add global state for app info (name, version)

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,7 +1,9 @@
 "use client";
 import { NavbarState } from "@/states/NavbarState";
+import { AppState } from "@/states/AppState";
 
 export default function Navbar() {
+  const appState = AppState();
   const navbarState = NavbarState();
 
   const handleToggleMenu = () => {
@@ -9,19 +11,24 @@ export default function Navbar() {
   };
 
   return (
-    <nav className="flex flex-row navbar bg-blue-ribbon-800 text-white p-4 items-center">
-      <button
-        className={`p-2 mx-2 border-white border-2 rounded-xl hover:ease-in-out hover:duration-100 user-select-none md:hover:bg-blue-ribbon-900`}
-        onClick={handleToggleMenu}
-      >
-        {/* eslint-disable-next-line @next/next/no-img-element */}
-        <img
-          src="/assets/icons/navbar/menu-burger-white.png"
-          className="w-4"
-          alt="menu"
-        />
-      </button>
-      <h1 className="mx-2 font-bold text-2xl">MIO Go</h1>
+    <nav className="flex flex-row navbar bg-blue-ribbon-800 text-white p-4 items-center justify-between select-none">
+      <div className="flex flex-row items-center">
+        <button
+          className={`p-2 mx-2 border-white border-2 rounded-xl hover:ease-in-out hover:duration-100 user-select-none md:hover:bg-blue-ribbon-900`}
+          onClick={handleToggleMenu}
+        >
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
+            src="/assets/icons/navbar/menu-burger-white.png"
+            className="w-4"
+            alt="menu"
+          />
+        </button>
+        <h1 className="mx-2 pt-1 font-bold text-2xl">{appState.appName}</h1>
+      </div>
+      <div className="mx-2 p-2 font-bold bg-white text-blue-ribbon-600 text-xs rounded-xl">
+        <p className="pt-1">v{appState.version}</p>
+      </div>
     </nav>
   );
 }
--- src/app/sobre/page.jsx
+++ src/app/sobre/page.jsx
@@ -1,11 +1,16 @@
+"use client";
+import { AppState } from "@/states/AppState";
+
 export default function Page() {
+  const appState = AppState();
+
   return (
     <div className="flex flex-1 flex-col p-8 w-full h-full items-center justify-center text-center select-none">
       <h1 className="text-4xl font-bold my-10 text-blue-ribbon-700">
         Sobre la app
       </h1>
       <p className="my-4">
-        <b className="text-blue-ribbon-700">MIO Go </b>
+        <b className="text-blue-ribbon-700">{appState.appName} </b>
         es un cliente <b>NO oficial,</b> gratuito y de{" "}
         <a
           href="https://github.com/JuanLoaiza007/mio-go"
